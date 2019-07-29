827efb00aee5e67b730c1a05c05bf63031b1f980
phi-tau <philiptaylor13@gmail.com>
2019-07-29 15:17:26 -0500

    fixed a file name

--- Week05c_RainyDays.java
+++ Week05c_RainyDays.java
@@ -1,11 +1,13 @@
 /** 
-
-
 Write a program RainyDays to compute and print the percentage of 
 days of rain in a year. Ask the user to input the city, the year 
 and the number or rainy days for that year. Print a message that 
 includes the year and the percentage of days that were rainy.
 
+Use a custom method called getRainyPercentage() that computes 
+the percentage with the parameters:   
+- year
+- rainy days
 
 E.g. What city?
 Denver
@@ -16,9 +18,9 @@ How many days of rain?
 It rained 12.8767% of the year in Denver in 2012 
 **/
 
-import java.util.Scanner;// using a file from the creator of the scanner
+import java.util.Scanner;
 
-public class RainyDays
+public class Week05c_RainyDays
 {
    public static void main (String[] args)
    {
